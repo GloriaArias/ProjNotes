3767da3f8888b33eca94ac4865ed460661c145e2
GloriaArias <ariasutreragloria@gmail.com>
2022-03-02 08:42:21 -0600

    app update to modern js

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 // Preambulo: Ayuda a manejar errores http
-var createError = require('http-errors');
+import createError from 'http-errors';
 // Ayuda a crear servidores web
-var express = require('express');
+import express from 'express';
 // Nucleo de node, ayuda al manejo de las rutas
-var path = require('path');
+import path from 'path';
 // Ayuda al manejo de las cookies
-var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
 // Maneja el log de peticiones http
-var logger = require('morgan');
+import logger from 'morgan';
 
 //Las rutas
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import aboutRouter from "./routes/about";
 
 // Aquí se crea la instancia de express
 //(req, res, next, err) => (...)
-var app = express();
+const app = express();
 
 // Configuración del motor de plantillas (template engine)
 // view engine setup
@@ -37,22 +37,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 
-
-
+//ctrl + k + c ---- Sirve para comentar en bloque
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ?
+ err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// render the error page
+res.status(err.status || 500);
+res.render('error');
 });
-
-module.exports = app;
+//Exportando instancia de app usando JS moderno
+export default app;
