fde3677edf5641e3f4cb90515e9cecdecdcaf482
GloriaArias <ariasutreragloria@gmail.com>
2022-03-16 08:44:17 -0600

    Se agrega module Resolver

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-import app from '../app';
+import app from '@S/app';
 //Modernizando el script
 //var debug = require('debug')('projnotes:server');
 import Debug from 'debug';
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let emojieDataset = ['😎','😛','🤩','👼','🎈','🎄','🎀','⏱','🥩','🏍','🚩','🌤','🌚','🌟','⚡','🌈','💙','🕳','💨','♻'];
+  let emojie = emojieDataset[Math.floor(Math.random()* emojieDataset.length)];
+  res.render('index',
+   { 
+     title: 'Express' ,
+     author: 'Arias Utrera Gloria',
+     emojie
+    });
 }); 
 
 module.exports = router;
