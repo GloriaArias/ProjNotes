33a4a874388da72d404a43f849632480b5c3a01e
GloriaArias <ariasutreragloria@gmail.com>
2022-03-30 07:55:45 -0600

    Testing de ES06 en Front-end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,25 @@
 console.log("🎁 Webpack Working!!!");
+
+// Default parameters
+let show = (m= "😝") => {
+    alert(m)
+};
+show();
+
+// Promises
+function resolveAfter2Seconds(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('function resolve')
+        }, 2000)
+    });
+}
+
+async function asyncCall() {
+    console.log("Calling asyn function!!!");
+    const result = await resolveAfter2Seconds();
+    console.log(result); // Imprime "function resolve" en la consola
+
+}
+
+asyncCall();
