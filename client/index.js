6c6a71b4de7d7c5b28dbe808b49a7eebb2523852
GloriaArias <ariasutreragloria@gmail.com>
2022-05-24 14:29:11 -0500

    S26 Finalizando Eslint Config

--- client/index.js
+++ client/index.js
@@ -1,28 +1,25 @@
-// Incorporando estilos a mi bundle
+// Incorporando estilos
+// a mi bundle
 import './styles/style.css'
 import './styles/mystyle.css'
-
-console.log("🎁 Webpack Working!!!");
-// Default parameters
-let show = (m= "😝") => {
-    console.log(m)
+console.log("🎁 Front-End Working!!!");
+// Default parameters ES6/2015
+let show = (m = "😝") => {
+  console.log(m);
 };
 show();
-
-// Promises
-function resolveAfter2Seconds(){
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('function resolve')
-        }, 2000)
-    });
+// Promises ES06
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("function resolve");
+    }, 10000);
+  });
 }
 
 async function asyncCall() {
-    console.log("Calling asyn function!!!");
-    const result = await resolveAfter2Seconds();
-    console.log(result); // Imprime "function resolve" en la consola
-
+  console.log("Calling async function!!!");
+  const result = await resolveAfter2Seconds();
+  console.log(result); // Imprime "function resolve" en la consola
 }
-
 asyncCall();
